e7f3f69fcb3f0140622f2447b79509247574b4db
AndresRicci93 <andresricci93@gmail.com>
2020-07-06 20:31:06 +0200

    added google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,26 @@ module.exports = {
    {
      resolve: `gatsby-plugin-mdx`,
      options: {
-       extension: [`.md`, `.mdx`]
-     }
-   }
-],
+       extensions: [`.md`, `.mdx`],
+       gatsbyRemarkPlugins: [
+         {
+           resolve: `gatsby-remark-images`,
+           options: {
+             maxWidth: 1200,
+           },
+         },
+       ],
+     },
+   },
+   {
+     resolve: `gatsby-plugin-google-fonts`,
+     options: {
+       fonts: [
+         `roboto`,
+         `muli\:400,400i,700,700i`,
+       ],
+       display: "swap",
+     },
+   },
+ ],
 }
